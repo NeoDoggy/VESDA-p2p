13e9ed5a613c7026a70fe7a1a59adeebdf0b3fd5
NeoDoggy <neodog0616@gmail.com>
2025-07-15 22:43:37 +0800

    [Main] update nodeID to Mac addr

---
+++ conf.secret.h
@@ -0,0 +1,3 @@
+char* host = "esp32";
+char* ssid = "hahapiyan";
+char* password = "12345678";
--- proto.ino
+++ proto.ino
@@ -1,229 +1,232 @@
 #include <WiFi.h>
+#include <esp_wifi.h>
+#include <esp_now.h>
 #include <WiFiClient.h>
 #include <WebServer.h>
 #include <ESPmDNS.h>
 #include <Update.h>
 #include <GP2YDustSensor.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-TaskHandle_t Task2;
+#include "conf.secret.h"
 
-const uint8_t SHARP_LED_PIN = 6;   // Sharp Dust/particle sensor Led Pin
-const uint8_t SHARP_VO_PIN = 7;    // Sharp Dust/particle analog out pin used for reading 
+
+#define SHARP_LED_PIN 6
+#define SHARP_VO_PIN 7
+#define SDA0_Pin 16
+#define SCL0_Pin 15
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define BUTTON_PIN 18
+
+
+uint8_t broadcastAddress[] = {0xC0, 0x49, 0xef, 0xd0, 0xd6, 0xb8}; //c0:49:ef:d0:d6:b8
+typedef struct p2pMsgStruct {
+    char nodeMac[18];
+    int curD;
+    int avgD;
+} p2pMsgStruct;
+
+
+p2pMsgStruct sendData;
+TaskHandle_t serverTaskHandler;
+
+esp_now_peer_info_t peerInfo;
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
 
 GP2YDustSensor dustSensor(GP2YDustSensorType::GP2Y1014AU0F, SHARP_LED_PIN, SHARP_VO_PIN);
- 
-char* host = "esp32";
-char* ssid = "*";
-char* password = "*";
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 WebServer server(80);
  
-String css =
-"<style>#file-input,input{width:100%;height:100px;border-radius:5px;margin:10px auto;font-size:16px}"
-"input{background:#fff;border:0;padding:0 15px}body{background:#5398D9;font-family:sans-serif;font-size:14px;color:#4d4d4d}"
-"#file-input{padding:0;border:2px solid #ddd;line-height:45px;text-align:left;display:block;cursor:pointer}"
-"#bar,#prgbar{background-color:#4d4d4d;border-radius:15px}#bar{background-color:#5398D9;width:0%;height:15px}"
-"form{background:#EDF259;max-width:300px;margin:80px auto;padding:30px;border-radius:5px;text-align:center}"
-".btn{background:#5398D9;color:#EDF259;cursor:pointer}</style>";
- 
-String loginIndex =
-"<form name='loginForm'>"
-"<h2>Chosemaker ESP32 Login</h2>"
-"<input name=userid placeholder='Username'> "
-"<input name=pwd placeholder='Password' type=Password> "
-"<input type=submit onclick=check(this.form) class=btn value=Login></form>"
-"<script>"
-    "function check(form)"
-    "{"
-    "if(form.userid.value=='admin' && form.pwd.value=='admin')"
-    "{"
-    "window.open('/serverIndex')"
-    "}"
-    "else"
-    "{"
-    " alert('Error Password or Username')/*displays error message*/"
-    "}"
-    "}"
-"</script>" + css;
- 
-String serverIndex =
- "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
- "<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
- "<input type='file' name='update'>"
- "<input type='submit' value='Update'>"
- "<div id='prg'>0%</div>"
- "<div id='prgbar'><div id='bar'></div></div></form>"
- "<script>"
-  "$('form').submit(function(e){"
-  "e.preventDefault();"
-  "var form = $('#upload_form')[0];"
-  "var data = new FormData(form);"
-  " $.ajax({"
-  "url: '/update',"
-  "type: 'POST',"
-  "data: data,"
-  "contentType: false,"
-  "processData:false,"
-  "xhr: function() {"
-  "var xhr = new window.XMLHttpRequest();"
-  "xhr.upload.addEventListener('progress', function(evt) {"
-  "if (evt.lengthComputable) {"
-  "var per = evt.loaded / evt.total;"
-  "$('#prg').html('Running ' + Math.round(per*100) + '%');"
-  "$('#bar').css('width',Math.round(per*100) + '%');"
-  "}"
-  "}, false);"
-  "return xhr;"
-  "},"
-  "success:function(d, s) {"
-  "console.log('success!')"
- "},"
- "error: function (a, b, c) {"
- "}"
- "});"
- "});"
- "</script>" + css;
+extern String css;
+extern String loginIndex;
+extern String serverIndex;
+
+bool hasOTA=false;
+
+uint8_t baseMac[6];
+char esp32Mac[18];
+
+void readMacAddress(){
+  esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
+  snprintf(esp32Mac,18,"%02x:%02x:%02x:%02x:%02x:%02x\n",
+                      baseMac[0], baseMac[1], baseMac[2],
+                      baseMac[3], baseMac[4], baseMac[5]);
+}
  
 void setup(void) {
   Serial.begin(115200);
-  // Connect to WiFi network
-  WiFi.begin(ssid, password);
-  Serial.println("");
- 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  Wire.begin(SDA0_Pin, SCL0_Pin);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
   }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
- 
-  if (!MDNS.begin(host)) { 
-    Serial.println("Error setting up MDNS responder!");
-    while (1) {
-      delay(1000);
+  display.setRotation(2);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  // Display static text
+  display.println("Starting UP");
+  display.println("Hold button to enter OTA");
+  display.display(); 
+
+  delay(3000);
+
+  if(digitalRead(BUTTON_PIN)==LOW){ // ota mode
+    hasOTA=true;
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("Entered OTA mode");
+    display.println("Waiting for connection to:");
+    display.println("SSID: "+String(ssid));
+    display.display(); 
+    // Connect to WiFi network
+    WiFi.begin(ssid, password);
+    Serial.println("");
+    // Wait for connection
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
     }
-  }
-  Serial.println("mDNS responder started");
-  /*return index page which is stored in serverIndex */
-  server.on("/", HTTP_GET, []() {
-    server.sendHeader("Connection", "close");
-    server.send(200, "text/html", loginIndex);
-  });
-  server.on("/serverIndex", HTTP_GET, []() {
-    server.sendHeader("Connection", "close");
-    server.send(200, "text/html", serverIndex);
-  });
-  /*handling uploading firmware file */
-  server.on("/update", HTTP_POST, []() {
-    server.sendHeader("Connection", "close");
-    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
-    ESP.restart();
-  }, []() {
-    HTTPUpload& upload = server.upload();
-    if (upload.status == UPLOAD_FILE_START) {
-      Serial.printf("Update: %s\n", upload.filename.c_str());
-      if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
-        Update.printError(Serial);
-      }
-    } else if (upload.status == UPLOAD_FILE_WRITE) {
-      /* flashing firmware to ESP*/
-      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
-        Update.printError(Serial);
-      }
-    } else if (upload.status == UPLOAD_FILE_END) {
-      if (Update.end(true)) { //true to set the size to the current progress
-        Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
-      } else {
-        Update.printError(Serial);
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setCursor(0, 0);
+    display.println("IP address: ");
+    display.println(WiFi.localIP());
+    display.display(); 
+
+    if (!MDNS.begin(host)) { 
+      Serial.println("Error setting up MDNS responder!");
+      while (1) {
+        delay(1000);
       }
     }
-  });
-  server.begin();
+    Serial.println("mDNS responder started");
+    /*return index page which is stored in serverIndex */
+    server.on("/", HTTP_GET, []() {
+      server.sendHeader("Connection", "close");
+      server.send(200, "text/html", loginIndex);
+    });
+    server.on("/serverIndex", HTTP_GET, []() {
+      server.sendHeader("Connection", "close");
+      server.send(200, "text/html", serverIndex);
+    });
+    /*handling uploading firmware file */
+    server.on("/update", HTTP_POST, []() {
+      server.sendHeader("Connection", "close");
+      server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+      ESP.restart();
+    }, []() {
+      HTTPUpload& upload = server.upload();
+      if (upload.status == UPLOAD_FILE_START) {
+        Serial.printf("Update: %s\n", upload.filename.c_str());
+        if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
+          Update.printError(Serial);
+        }
+      } else if (upload.status == UPLOAD_FILE_WRITE) {
+        /* flashing firmware to ESP*/
+        if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+          Update.printError(Serial);
+        }
+      } else if (upload.status == UPLOAD_FILE_END) {
+        if (Update.end(true)) { //true to set the size to the current progress
+          Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+        } else {
+          Update.printError(Serial);
+        }
+      }
+    });
+    server.begin();
+  }
+  else{ // p2p
+    WiFi.mode(WIFI_STA);
+    if (esp_now_init() != ESP_OK) {
+      Serial.println("Error initializing ESP-NOW");
+      return;
+    }
+    esp_now_register_send_cb(OnDataSent);
+    memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+    peerInfo.channel = 0;  
+    peerInfo.encrypt = false;
+    if (esp_now_add_peer(&peerInfo) != ESP_OK){
+      Serial.println("Failed to add peer");
+      return;
+    }
+  }
+
+  readMacAddress();
 
   dustSensor.setBaseline(0); // set no dust voltage according to your own experiments
   dustSensor.setCalibrationFactor(1.1); // calibrate against precision instrument
   dustSensor.begin();
 
   xTaskCreatePinnedToCore(
-                    loop1,       /* Task function. */
-                    "loop1",     /* name of task. */
+                    serverHandleLoop,       /* Task function. */
+                    "serverHandleLoop",     /* name of task. */
                     10000,       /* Stack size of task */
                     NULL,        /* parameter of the task */
                     1,           /* priority of the task */
-                    &Task2,      /* Task handle to keep track of created task */
+                    &serverTaskHandler,      /* Task handle to keep track of created task */
                     0);          /* pin task to core 0 */
+
+
 }
 
-void loop1(void * pvParameters ){
-  while(1){
-    Serial.print("Dust density: ");
-    Serial.print(dustSensor.getDustDensity());
-    Serial.print(" ug/m3; Running average: ");
-    Serial.print(dustSensor.getRunningAverage());
-    Serial.println(" ug/m3");
-    delay(1000);
+// 1KHZ loop
+void serverHandleLoop(void * pvParameters ){while(1){
+  if(hasOTA){
+    server.handleClient();
   }
-}
+
+delay(1);}}
  
 void loop(void) {
-  server.handleClient();
-  delay(1);
+  int dd=dustSensor.getDustDensity();
+  int ddavg=dustSensor.getRunningAverage();
+  Serial.print("Dust density: ");
+  Serial.print(dd);
+  Serial.print(" ug/m3; Running average: ");
+  Serial.print(ddavg);
+  Serial.println(" ug/m3");
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+  if(hasOTA){
+    display.println("IP address: ");
+    display.println(WiFi.localIP());
+    display.println("---------------------");
+  }
+  display.println("Density: "+String(dd)+" ug/m3");
+  display.println("Running AVG: "+String(ddavg)+" ug/m3");
+  display.println("---------------------");
+  display.println("MAC:");
+  display.println(esp32Mac);
+  display.display();
+  if(!hasOTA){
+    strcpy(sendData.nodeMac, esp32Mac);
+    sendData.curD=dd;
+    sendData.avgD=ddavg;
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &sendData, sizeof(sendData));
+    if (result == ESP_OK) {
+      Serial.println("Sent with success");
+    }
+    else {
+      Serial.println("Error sending the data");
+    }
+  }
+  delay(1000);
 }
-
-/*
-<style>
-    #file-input,
-    input {
-        width: 100%;
-        height: 100px;
-        border-radius: 5px;
-        margin: 10px auto;
-        font-size: 16px
-    }
-    input {
-        background: #fff;
-        border: 0;
-        padding: 0 15px
-    }
-    body {
-        background: #1a1a1a;
-        font-family: sans-serif;
-        font-size: 14px;
-        color: #FdFdFd
-    }
-    #file-input {
-        padding: 0;
-        border: 2px solid #ddd;
-        line-height: 45px;
-        text-align: left;
-        display: block;
-        cursor: pointer
-    }
-    #bar,
-    #prgbar {
-        background-color: #4d4d4d;
-        border-radius: 15px
-    }
-    #bar {
-        background-color: #4cdebc;
-        width: 0%;
-        height: 15px
-    }
-    form {
-        background: #1f1f1f;
-        max-width: 300px;
-        margin: 80px auto;
-        padding: 30px;
-        border-radius: 5px;
-        text-align: center
-    }
-    .btn {
-        background: #4a4a4a;
-        color: #FFFFFF;
-        cursor: pointer
-    }
-</style>
-*/
